8e4a950df21423196b46b188dda9916992e29126
biQte <patrykgonet@gmail.com>
2021-11-19 19:48:39 +0100

    Add color constants

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const DarkBlue = Color(0x22577AFF);
+const Turquoise = Color(0x38A3A5FF);
+const TextColor = Color(0xEEEEEEFF);
+const BackgroundColor = Color(0x373A40FF);
